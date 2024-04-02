f7d4ad3958826c01883643f5128c79a92339f346
siapri21 <siapriouattara21@gmail.com>
2024-04-02 21:47:26 +0200

    modification

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,30 +1,30 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const gallery = document.querySelector('.gallery');
-    let scrollAmount = 0;
-    let isHovering = false;
-    let scrollInterval;
+// document.addEventListener("DOMContentLoaded", function() {
+//     const gallery = document.querySelector('.gallery');
+//     let scrollAmount = 0;
+//     let isHovering = false;
+//     let scrollInterval;
   
-    function scrollGallery() {
-      if (isHovering) {
-        scrollAmount += 10; // Changer cette valeur pour ajuster la vitesse du défilement
-        if (scrollAmount > gallery.scrollWidth - gallery.offsetWidth - 2) {
-          // Si le défilement dépasse le bord droit de la dernière image, revenir au début
-          scrollAmount = 0;
-        }
-        gallery.style.transform = `translateX(-${scrollAmount}px)`;
-      }
-    }
+//     function scrollGallery() {
+//       if (isHovering) {
+//         scrollAmount += 10; // Changer cette valeur pour ajuster la vitesse du défilement
+//         if (scrollAmount > gallery.scrollWidth - gallery.offsetWidth - 2) {
+//           // Si le défilement dépasse le bord droit de la dernière image, revenir au début
+//           scrollAmount = 0;
+//         }
+//         gallery.style.transform = `translateX(-${scrollAmount}px)`;
+//       }
+//     }
   
-    // Déclencher le défilement lorsque la souris survole la galerie
-    gallery.addEventListener('mouseenter', function() {
-      isHovering = true;
-      scrollInterval = setInterval(scrollGallery, 50); // Interval plus court pour un défilement plus lent
-    });
+//     // Déclencher le défilement lorsque la souris survole la galerie
+//     gallery.addEventListener('mouseenter', function() {
+//       isHovering = true;
+//       scrollInterval = setInterval(scrollGallery, 50); // Interval plus court pour un défilement plus lent
+//     });
   
-    // Arrêter le défilement lorsque la souris quitte la galerie
-    gallery.addEventListener('mouseleave', function() {
-      isHovering = false;
-      clearInterval(scrollInterval); // Arrêter l'intervalle lors de la sortie de la souris
-    });
-  });
+//     // Arrêter le défilement lorsque la souris quitte la galerie
+//     gallery.addEventListener('mouseleave', function() {
+//       isHovering = false;
+//       clearInterval(scrollInterval); // Arrêter l'intervalle lors de la sortie de la souris
+//     });
+//   });
   
